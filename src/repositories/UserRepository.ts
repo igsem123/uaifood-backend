08d01e92bc0d1b531aeaf408ecb88839ae5b8604
igsem123 <rnathanmoreira@gmail.com>
2025-10-29 21:44:25 -0300

    Feature: add user structure with Prisma integration and basic CRUD operations; configure Tsyringe for dependecy injection;

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,33 @@
+import { prisma } from "../config/prisma";
+import { User } from "@prisma/client";
+
+const UserRepository = {
+    async createUser(data: Omit<User, "id" | "createdAt" | "updatedAt">): Promise<User> {
+        return prisma.user.create({
+            data,
+        });
+    },
+    async getUserById(id: number): Promise<User | null> {
+        return prisma.user.findUnique({
+            where: { id },
+        });
+    },
+    async getUserByEmail(email: string): Promise<User | null> {
+        return prisma.user.findUnique({
+            where: { email },
+        });
+    },
+    async updateUser(id: number, data: Partial<Omit<User, "id">>): Promise<User> {
+        return prisma.user.update({
+            where: { id },
+            data,
+        });
+    },
+    async deleteUser(id: number): Promise<User> {
+        return prisma.user.delete({
+            where: { id },
+        });
+    }
+}
+
+export default UserRepository;
---
+++ src/services/UserService.ts
@@ -0,0 +1,34 @@
+import userRepository from "../repositories/UserRepository";
+import {User} from "@prisma/client";
+import { injectable } from "tsyringe";
+
+@injectable()
+export class UserService {
+    registerUser = async (data: User): Promise<User> => {
+        const existingUser = await userRepository.getUserByEmail(data.email);
+        if (existingUser) {
+            throw new Error('Email already in use');
+        }
+        return await userRepository.createUser(data);
+    }
+
+    findUserById = async (id: number): Promise<User | null> => {
+        return await userRepository.getUserById(id);
+    }
+
+    updateUser = async (id: number, data: Partial<Omit<User, "id">>): Promise<User> => {
+        const user = await userRepository.getUserById(id);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        return await userRepository.updateUser(id, data);
+    }
+
+    deleteUser = async (id: number): Promise<User> => {
+        const user = await userRepository.getUserById(id);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        return await userRepository.deleteUser(id);
+    }
+}
